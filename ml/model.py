54e0d36b1aeda88473560a1b208f3616b44a7b60
raqmun92 <rmuno@wgu.edu>
2024-03-30 14:09:33 +0000

    Fixed missing commas in hyperparameters

--- ml/model.py
+++ ml/model.py
@@ -25,10 +25,10 @@ def train_model(X_train, y_train):
 
     # Define the hyperparameters for model testing
     hyperparameters = {
-        'n_estimators': [50, 100, 150, 200]
-        'max_depth': [5, 10, 15, None]
-        'min_samples_split': [2, 5, 10, 20]
-        'min_samples_leaf': [1, 5, 10, 20]
+        'n_estimators': [50, 100, 150, 200],
+        'max_depth': [5, 10, 15, None],
+        'min_samples_split': [2, 5, 10, 20],
+        'min_samples_leaf': [1, 5, 10, 20],
         'learning_rate': [0.001, 0.01, 0.1, 1.0]
     }
 
