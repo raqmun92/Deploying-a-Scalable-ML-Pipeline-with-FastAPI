e6b02c6810ebcb31bab77e20ca6846e5ec95d479
raqmun92 <rmuno@wgu.edu>
2024-03-31 20:42:04 +0000

    Added error handling code

--- local_api.py
+++ local_api.py
@@ -3,13 +3,18 @@ import json
 import requests
 
 # Send a GET using the URL http://127.0.0.1:8000
-r = requests.get("http://127.0.0.1:8000")
+try:
+    r = requests.get("http://127.0.0.1:8000")
+    r.raise_for_status()
 
-# Print the status code
-print('Status code:', r.status_code)
+    # Print the status code
+    print('Status code:', r.status_code)
 
-# Print the welcome message
-print("Welcome message: ", r.json()["greeting"])
+    # Print the welcome message
+    print("Welcome message: ", r.json()["greeting"])
+
+except requests.exceptions.RequestException as e:
+    print("Error occurred during request:", e)
 
 
 
@@ -30,11 +35,15 @@ data = {
     "native-country": "United-States",
 }
 
-# Send a POST using the data above
-r = requests.post("http://127.0.0.1:8000/data/", json=data)
+try:
+    # Send a POST using the data above
+    r = requests.post("http://127.0.0.1:8000/data/", json=data)
+    
+    # Print the status code
+    print("Status code:", r.status_code)
+    
+    # Print the result
+    print("Result:", r.json()["result"])
 
-# Print the status code
-print("Status code:", r.status_code)
-
-# Print the result
-print("Result:", r.json()["result"])
+except requests.exceptions.RequestException as e:
+    print("Error occurred during request", e)
